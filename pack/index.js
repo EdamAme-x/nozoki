f5e2346ecc67d426bde099efd39c2083d298e32c
EdamAme-x <121654029+EdamAme-x@users.noreply.github.com>
2023-11-14 00:31:05 +0000

    add js

--- pack/index.js
+++ pack/index.js
@@ -4,4 +4,15 @@
 */
 console.log("Welcome to Nozoki OC / Frontend by @amex2189");
 
+let password = "";
+let target = "";
+
+function getTicket(url) {
+  if (url.includes("?")) {
+      url = url.split("?")[0];
+  }
+  let ticket = url.split("/").pop();
+  return ticket;
+}
+
 $("#app").in(div({}, "hi!"));
