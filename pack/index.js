9dadadb7784fd192f4bed99e107ee8eb10555411
EdamAme-x <121654029+EdamAme-x@users.noreply.github.com>
2023-11-14 03:04:29 +0000

    wip

--- pack/index.js
+++ pack/index.js
@@ -2,11 +2,12 @@
   Created by @amex2189
   @Obstruction
 */
+import { tw } from "https://cdn.skypack.dev/twind";
+
 console.log("Welcome to Nozoki OC / Frontend by @amex2189");
 
-let password = "";
 let target = "";
-let api = "https://tumuri.deno.dev";
+let api = "https://tumuri.deno.dev/";
 
 function getTicket(url) {
   if (url.includes("?")) {
@@ -16,6 +17,52 @@ function getTicket(url) {
   return ticket;
 }
 
+async function getLastMessage(url) {
+  const ticket = getTicket(url);
+  const res = await fetch(api + ticket);
+  const data = res.json();
+
+  return data;
+}
+
+const consts = {
+  creater: "ame_x (@amex2189) & Piloking",
+};
+
 window.onload = function () {
-  $("#app").in(div({}, "hi!!"));
+  $("#app").in(div(
+    {
+      class: tw("flex flex-col items-center w-full h-screen p-4 bg-gray-900"),
+    },
+    div(
+      {
+        class: tw("mb-5"),
+      },
+      h1({
+        class: tw("text-3xl"),
+      }, "OC Observer"),
+    ),
+    div(
+      {
+        class: tw("mb-4"),
+      },
+      input({
+        $input: (e) => {
+          console.log("URL: " + e.target.value);
+          target = e.target.value;
+        },
+        placeholder: "~/ti/g2/~",
+        class: tw("rounded text-xl text-black border-none nuem-x"),
+      }),
+    ),
+    div({
+      class: tw("h-full overflow-y-scroll log-x"),
+    }, "{ ここにチャットログ }"),
+    div(
+      {},
+      p({
+        class: "",
+      }, "&copy; " + consts.creater),
+    ),
+  ));
 };
